05c7337d29c7ed251f24d621fd87cec8dc037bd7
Ishan Ranasingh <44061541+truthfool@users.noreply.github.com>
2023-01-25 12:46:21 +0530

    gsma transfer dto (#16)

---
+++ src/main/java/org/mifos/connector/common/gsma/dto/CustomData.java
@@ -0,0 +1,15 @@
+package org.mifos.connector.common.gsma.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class CustomData {
+    public String key;
+    public String value;
+}
