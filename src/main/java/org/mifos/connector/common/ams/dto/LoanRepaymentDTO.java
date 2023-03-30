479fd5f803f8a3b71b1c9a9e837239745478ee53
Ishan Ranasingh <44061541+truthfool@users.noreply.github.com>
2023-03-30 12:08:22 +0530

    Added getters and setters (#25)
    * added getters and setters
    
    * added getters and setters

--- src/main/java/org/mifos/connector/common/ams/dto/LoanRepaymentDTO.java
+++ src/main/java/org/mifos/connector/common/ams/dto/LoanRepaymentDTO.java
@@ -2,13 +2,6 @@ package org.mifos.connector.common.ams.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.*;
-
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@ToString
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class LoanRepaymentDTO {
     @JsonProperty("transactionDate")
@@ -21,4 +14,65 @@ public class LoanRepaymentDTO {
     public String locale;
     @JsonProperty("dateFormat")
     public String dateFormat;
+    @Override
+    public String toString() {
+        return "LoanRepaymentDTO{" +
+                "transactionDate='" + transactionDate + '\'' +
+                ", paymentTypeId='" + paymentTypeId + '\'' +
+                ", transactionAmount='" + transactionAmount + '\'' +
+                ", locale='" + locale + '\'' +
+                ", dateFormat='" + dateFormat + '\'' +
+                '}';
+    }
+
+    public String getTransactionDate() {
+        return transactionDate;
+    }
+
+    public void setTransactionDate(String transactionDate) {
+        this.transactionDate = transactionDate;
+    }
+
+    public String getPaymentTypeId() {
+        return paymentTypeId;
+    }
+
+    public void setPaymentTypeId(String paymentTypeId) {
+        this.paymentTypeId = paymentTypeId;
+    }
+
+    public String getTransactionAmount() {
+        return transactionAmount;
+    }
+
+    public void setTransactionAmount(String transactionAmount) {
+        this.transactionAmount = transactionAmount;
+    }
+
+    public String getLocale() {
+        return locale;
+    }
+
+    public void setLocale(String locale) {
+        this.locale = locale;
+    }
+
+    public String getDateFormat() {
+        return dateFormat;
+    }
+
+    public void setDateFormat(String dateFormat) {
+        this.dateFormat = dateFormat;
+    }
+
+    public LoanRepaymentDTO() {
+    }
+
+    public LoanRepaymentDTO(String transactionDate, String paymentTypeId, String transactionAmount, String locale, String dateFormat) {
+        this.transactionDate = transactionDate;
+        this.paymentTypeId = paymentTypeId;
+        this.transactionAmount = transactionAmount;
+        this.locale = locale;
+        this.dateFormat = dateFormat;
+    }
 }
