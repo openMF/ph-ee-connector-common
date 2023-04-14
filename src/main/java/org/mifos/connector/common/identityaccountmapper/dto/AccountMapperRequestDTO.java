da719911dca6b025e2833e56737040a48c58d6a4
apurbraj <apurb@fynarfin.io>
2023-04-14 11:28:57 +0530

    Identity Account Mapper DTO

---
+++ src/main/java/org/mifos/connector/common/identityaccountmapper/dto/AccountMapperRequestDTO.java
@@ -0,0 +1,39 @@
+package org.mifos.connector.common.identityaccountmapper.dto;
+
+import java.util.List;
+
+public class AccountMapperRequestDTO {
+    private String requestID;
+    private String sourceBBID;
+    private List< BeneficiaryDTO > beneficiaries ;
+
+    public AccountMapperRequestDTO(String requestID, String sourceBBID, List<BeneficiaryDTO> beneficiaries) {
+        this.requestID = requestID;
+        this.sourceBBID = sourceBBID;
+        this.beneficiaries = beneficiaries;
+    }
+
+    public String getRequestID() {
+        return requestID;
+    }
+
+    public void setRequestID(String requestID) {
+        this.requestID = requestID;
+    }
+
+    public String getSourceBBID() {
+        return sourceBBID;
+    }
+
+    public void setSourceBBID(String sourceBBID) {
+        this.sourceBBID = sourceBBID;
+    }
+
+    public List<BeneficiaryDTO> getBeneficiaries() {
+        return beneficiaries;
+    }
+
+    public void setBeneficiaries(List<BeneficiaryDTO> beneficiaries) {
+        this.beneficiaries = beneficiaries;
+    }
+}
---
+++ src/main/java/org/mifos/connector/common/identityaccountmapper/dto/PaymentModalityDTO.java
@@ -0,0 +1,37 @@
+package org.mifos.connector.common.identityaccountmapper.dto;
+
+public class PaymentModalityDTO {
+    private String paymentModality;
+    private String financialAddress;
+    private String bankingInstitutionCode;
+
+    public PaymentModalityDTO(String paymentModality, String financialAddress, String bankingInstitutionCode) {
+        this.paymentModality = paymentModality;
+        this.financialAddress = financialAddress;
+        this.bankingInstitutionCode = bankingInstitutionCode;
+    }
+
+    public String getPaymentModality() {
+        return paymentModality;
+    }
+
+    public void setPaymentModality(String paymentModality) {
+        this.paymentModality = paymentModality;
+    }
+
+    public String getFinancialAddress() {
+        return financialAddress;
+    }
+
+    public void setFinancialAddress(String financialAddress) {
+        this.financialAddress = financialAddress;
+    }
+
+    public String getBankingInstitutionCode() {
+        return bankingInstitutionCode;
+    }
+
+    public void setBankingInstitutionCode(String bankingInstitutionCode) {
+        this.bankingInstitutionCode = bankingInstitutionCode;
+    }
+}
