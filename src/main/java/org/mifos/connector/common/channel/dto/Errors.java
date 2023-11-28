7c03bf08e077810ee9156b0147e5160fd68ac8e3
Sourabh <sourabh@Sourabhs-MacBook-Pro.local>
2023-11-28 17:45:05 +0530

    [PHEE-492] Modifying ErrorDTO for Error Aggregation

--- src/main/java/org/mifos/connector/common/channel/dto/Errors.java
+++ src/main/java/org/mifos/connector/common/channel/dto/Errors.java
@@ -1,6 +1,8 @@
 package org.mifos.connector.common.channel.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 import org.mifos.connector.common.gsma.dto.ErrorParameter;
@@ -10,9 +12,11 @@ import java.util.List;
 @Getter
 @Setter
 @ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class Errors {
     public String errorCategory;
     public String errorCode;
     public String errorDescription;
-    public List<org.mifos.connector.common.gsma.dto.ErrorParameter> errorParameters = null;
+    public List<ErrorParameter> errorParameters;
 }
