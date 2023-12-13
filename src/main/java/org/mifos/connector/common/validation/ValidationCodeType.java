d80dd3d0a4a47520e9aca57fb0d0694e60e9e456
Sourabh Kumar <148945054+shkr7@users.noreply.github.com>
2023-12-13 21:14:38 +0530

    [PHEE-505] Add PaymentHub Error Codes (#51)
    * Add PH Error Codes
    
    * Add PH Error Codes
    
    * Modify ValidationCodeTypes
    
    * Modified ValidationCodeType
    
    * Modified ValidationCodeType
    
    * Modified ValidationCodeType
    
    ---------
    
    Co-authored-by: Sourabh <sourabh@Sourabhs-MacBook-Pro.local>

--- src/main/java/org/mifos/connector/common/validation/ValidationCodeType.java
+++ src/main/java/org/mifos/connector/common/validation/ValidationCodeType.java
@@ -1,6 +1,8 @@
 package org.mifos.connector.common.validation;
 
 public interface ValidationCodeType {
+
     String getCode();
+    String getCategory();
     String getMessage();
 }
--- src/main/java/org/mifos/connector/common/validation/ValidationEnums.java
+++ src/main/java/org/mifos/connector/common/validation/ValidationEnums.java
@@ -1,22 +1,31 @@
 package org.mifos.connector.common.validation;
 
+import org.mifos.connector.common.exception.PaymentHubErrorCategory;
+
 public enum ValidationEnums implements ValidationCodeType {
     INVALID_LENGTH("INVALID_LENGTH", "length is invalid"),
     INVALID_LIST("INVALID_LIST", "list is invalid"),
-    INVALID_NEGATIVE_FIELD("INVALID_NEGATIVE_FIELD", "this field cannot be negative"),
+    INVALID_NEGATIVE_FIELD("INVALID_NEGATIVE_FIELD",  "this field cannot be negative"),
     INVALID_MAX_LENGTH("INVALID_MAX_LENGTH", "cannot exceed the maximum length");
 
     private final String code;
+    private final String category;
     private final String message;
 
     ValidationEnums(String code, String message) {
         this.code = code;
+        this.category = PaymentHubErrorCategory.Validation.toString();
         this.message = message;
     }
 
     @Override
     public String getCode() {
         return this.code;
+    }
+
+    @Override
+    public String getCategory() {
+        return this.category;
     }
 
     @Override
