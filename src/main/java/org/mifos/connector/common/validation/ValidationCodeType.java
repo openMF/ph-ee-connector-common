0df1e9cdbe2adcdda33bf18d62007baeb53f45ec
Sourabh <sourabh@Sourabhs-MacBook-Pro.local>
2023-12-13 19:26:49 +0530

    Modified ValidationCodeType

--- src/main/java/org/mifos/connector/common/validation/ValidationCodeType.java
+++ src/main/java/org/mifos/connector/common/validation/ValidationCodeType.java
@@ -3,9 +3,8 @@ package org.mifos.connector.common.validation;
 import org.mifos.connector.common.exception.PaymentHubErrorCategory;
 
 public interface ValidationCodeType {
+
     String getCode();
-    default String getCategory(){
-        return PaymentHubErrorCategory.Validation.toString();
-    }
+    String getCategory();
     String getMessage();
 }
--- src/main/java/org/mifos/connector/common/validation/ValidationEnums.java
+++ src/main/java/org/mifos/connector/common/validation/ValidationEnums.java
@@ -3,18 +3,18 @@ package org.mifos.connector.common.validation;
 import org.mifos.connector.common.exception.PaymentHubErrorCategory;
 
 public enum ValidationEnums implements ValidationCodeType {
-    INVALID_LENGTH("INVALID_LENGTH", PaymentHubErrorCategory.Validation.toString(), "length is invalid"),
-    INVALID_LIST("INVALID_LIST", PaymentHubErrorCategory.Validation.toString(), "list is invalid"),
-    INVALID_NEGATIVE_FIELD("INVALID_NEGATIVE_FIELD", PaymentHubErrorCategory.Validation.toString(), "this field cannot be negative"),
-    INVALID_MAX_LENGTH("INVALID_MAX_LENGTH", PaymentHubErrorCategory.Validation.toString(), "cannot exceed the maximum length");
+    INVALID_LENGTH("INVALID_LENGTH", "length is invalid"),
+    INVALID_LIST("INVALID_LIST", "list is invalid"),
+    INVALID_NEGATIVE_FIELD("INVALID_NEGATIVE_FIELD",  "this field cannot be negative"),
+    INVALID_MAX_LENGTH("INVALID_MAX_LENGTH", "cannot exceed the maximum length");
 
     private final String code;
     private final String category;
     private final String message;
 
-    ValidationEnums(String code, String category, String message) {
+    ValidationEnums(String code, String message) {
         this.code = code;
-        this.category = category;
+        this.category = PaymentHubErrorCategory.Validation.toString();
         this.message = message;
     }
 
